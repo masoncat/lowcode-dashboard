19d3b4765228f3d90a2b71e2165a8807a5aa5251
masoncat <threehxhot@163.com>
2023-07-16 11:58:40 +0800

    feat: lerna init

---
+++ packages/lowcode-demo/__tests__/lowcode-demo.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const lowcodeDemo = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(lowcodeDemo(), 'Hello from lowcodeDemo');
+console.info('lowcodeDemo tests passed');
