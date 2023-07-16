638d06db1a7e0f500293e3b96cdad7857a97abc5
masoncat <threehxhot@163.com>
2023-07-16 11:55:01 +0800

    feat: react18+webpack5

---
+++ packages/lowcode-runtime/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface IProps{}
+
+interface IState{}
+
+export default class App extends React.Component<IProps, IState>{
+    render(){
+        return 'hello react'
+    }
+}
